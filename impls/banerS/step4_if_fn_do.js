35f42aa3ffd10fa2430de0e96b958b9e24daaa61
souravwithstars <99480048+souravwithstars@users.noreply.github.com>
2023-05-31 14:39:46 +0530

    Step 4: Equality check for banerS

--- impls/banerS/step4_if_fn_do.js
+++ impls/banerS/step4_if_fn_do.js
@@ -25,15 +25,44 @@ const oneValueDivision = number => new MalPrimitive(`1/${number.value}`);
 const generalDivision = (firstNum, secondNum) =>
   new MalPrimitive(firstNum.value / secondNum.value);
 
+const oneValueEquals = value => new MalBool(true);
+const generalEquals = (firstValue, secondValue) =>
+  firstValue.value === secondValue.value;
+
+const generalGreaterEquals = (firstValue, secondValue) =>
+  firstValue.value >= secondValue.value;
+
+const generalLesserEquals = (firstValue, secondValue) =>
+  firstValue.value <= secondValue.value;
+
+const generalGreaterThan = (firstValue, secondValue) =>
+  firstValue.value < secondValue.value;
+
+const generalLesserThan = (firstValue, secondValue) =>
+  firstValue.value < secondValue.value;
+
+const equalityCheck = (genFn, ...args) => {
+  for (let i = 0; i < args.length - 1; i++) {
+    if (!genFn(...args.slice(i, i + 2))) return false;
+  }
+  return true;
+};
+
 const FNMAP = {
   'add': { genFn: generalAddition, oneValueFn: identityFn },
   'sub': { genFn: generalSubstraction, oneValueFn: oneValueSubstraction },
   'mul': { genFn: generalMultiplication, oneValueFn: identityFn },
-  'div': { genFn: generalDivision, oneValueFn: oneValueDivision }
+  'div': { genFn: generalDivision, oneValueFn: oneValueDivision },
+  'equals': { genFn: generalEquals, oneValueFn: oneValueEquals },
+  'greaterEquals': { genFn: generalGreaterEquals, oneValueFn: oneValueEquals },
+  'lesserEquals': { genFn: generalLesserEquals, oneValueFn: oneValueEquals },
+  'greaterThan': { genFn: generalGreaterThan, oneValueFn: oneValueEquals },
+  'lesserThan': { genFn: generalLesserThan, oneValueFn: oneValueEquals }
 };
 
 const operate = (operation, initialNumber, ...args) => {
   const { genFn, oneValueFn } = FNMAP[operation];
+  const equalityOp = ['equals', 'greaterEquals', 'lesserEquals', 'greaterThan', 'lesserThan'];
   if (initialNumber === undefined) {
     if (operation === 'add') return oneValueFn(new MalPrimitive(0))
     if (operation === 'mul') return oneValueFn(new MalPrimitive(1));
@@ -41,6 +70,8 @@ const operate = (operation, initialNumber, ...args) => {
     throw 'Wrong Number of args [0]';
   };
   if (args.length === 0) return oneValueFn(initialNumber);
+
+  if (equalityOp.includes(operation)) return new MalBool(equalityCheck(genFn, initialNumber, ...args));
 
   return args.reduce(genFn, initialNumber);
 };
@@ -92,6 +123,7 @@ const handle_do = (ast, env) => {
 
 const handle_if = (ast, env) => {
   const [_, condition, ifPart, elsePart] = ast.value;
+  const elsePartValue = elsePart ? EVAL(elsePart, env) : new MalNil();
   const evaluatedValue = EVAL(condition, env);
   let result;
 
@@ -100,7 +132,7 @@ const handle_if = (ast, env) => {
   } else if (evaluatedValue instanceof MalPrimitive) {
     result = new MalBool(true);
   }
-  return result.value ? EVAL(ifPart, env) : EVAL(elsePart, env);
+  return result.value ? EVAL(ifPart, env) : elsePartValue;
 };
 
 const READ = str => read_str(str);
@@ -130,6 +162,11 @@ env.set(new MalSymbol('+'), (...args) => operate('add', ...args));
 env.set(new MalSymbol('-'), (...args) => operate('sub', ...args));
 env.set(new MalSymbol('*'), (...args) => operate('mul', ...args));
 env.set(new MalSymbol('/'), (...args) => operate('div', ...args));
+env.set(new MalSymbol('='), (...args) => operate('equals', ...args));
+env.set(new MalSymbol('>='), (...args) => operate('greaterEquals', ...args));
+env.set(new MalSymbol('<='), (...args) => operate('lesserEquals', ...args));
+env.set(new MalSymbol('>'), (...args) => operate('lesserThan', ...args));
+env.set(new MalSymbol('<'), (...args) => operate('greaterThan', ...args));
 
 const rep = str => PRINT(EVAL(READ(str), env));
 
